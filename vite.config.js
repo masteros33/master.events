cb5872ccfedc56ef5715fff12916ab520893bf32
jude Obodai Sai <judeobodaisai@gmail.com>
2026-03-21 02:58:17 +0600

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Master Events',
-        short_name: 'MasterEvents',
-        description: "Ghana's Premier Ticketing Platform",
-        theme_color: '#f5a623',
-        background_color: '#110900',
-        display: 'standalone',
-        orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
   ],
   server: {
     host: '0.0.0.0',
